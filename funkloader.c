52897038deea9b394ffccc155c3542ede2b7e9c1
Stefan Siegl <stesie@wuchtbrumme.metafnord.de>
2008-03-14 23:40:37 +0100

    initial commit of initialization code.

---
+++ funkloader.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright(C) Benedikt K.
+ * Copyright(C) Juergen Eckert
+ * Copyright(C) 2007 Ulrich Radig <mail@ulrichradig.de>
+ * Copyright(C) 2007 Jochen Roessner <jochen@lugrot.de>
+ * Copyright(C) 2007,2008 Stefan Siegl <stesie@brokenpipe.de>
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "pinconfig.h"
+
+#define noinline __attribute__((noinline))
+
+extern unsigned short rfm12_trans (unsigned short);
+
+
+static void
+spi_init (void)
+{
+  /* configure MOSI, SCK, lines as outputs */
+  SPI_DDR |= _BV(SPI_MOSI) | _BV(SPI_SCK);
+  SPI_DDR &= ~_BV(SPI_MISO);
+
+  /* initialize spi link to rfm12 module */
+  SPI_CS_RFM12_DDR |= _BV(SPI_CS_RFM12);
+
+  /* Enable the pullup */
+  SPI_CS_RFM12_PORT |= _BV(SPI_CS_RFM12);
+
+  /* enable spi, set master and clock modes (f/2) */
+  SPCR = _BV(SPE) | _BV(MSTR);
+  SPSR = _BV(SPI2X);
+}
+
+
+static void
+rfm12_init (void)
+{
+  uint8_t i;
+
+  for (i = 0; i < 10; i ++)
+    _delay_ms (10);		/* wait until POR done */
+
+  rfm12_trans (0xC0E0);		/* AVR CLK: 10MHz */
+  rfm12_trans (0x80D7);		/* Enable FIFO */
+  rfm12_trans (0xC2AB);		/* Data Filter: internal */
+  rfm12_trans (0xCA81);		/* Set FIFO mode */
+  rfm12_trans (0xE000);		/* disable wakeuptimer */
+  rfm12_trans (0xC800);		/* disable low duty cycle */
+  rfm12_trans (0xC4F7);	        /* autotuning: -10kHz...+7,5kHz */
+  rfm12_trans (0x0000);
+
+  rfm12_trans (0xa620);		/* rfm12_setfreq(RFM12FREQ(433.92)) */
+  rfm12_trans (0x94ac);		/* rfm12_setbandwidth(5, 1, 4); */
+  rfm12_trans (0xc609);		/* rfm12_setbaud(34482); */
+  rfm12_trans (0x9820);		/* rfm12_setpower(0, 2); */
+}
+
+
+int
+main (void)
+{
+  rfm12_init ();
+
+}
